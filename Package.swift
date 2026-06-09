6cddb141129c4112ac9a7eb95e384f2f2ec99471
adgem-developers <developers@adgem.com>
2026-06-09 06:04:43 +0000

    Release 2.4.1

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "AdGem",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "AdGem", targets: ["AdGemSdk"])
     ],
     targets: [
         .binaryTarget(
             name: "AdGemSdk",
-            url: "https://adgem-framework.s3.amazonaws.com/iOS-2.4.0.zip",
-            checksum: "1cbc21bbbf39464245fc16a14c97bfe8c1572e1d0aab24c04bc210c459093ec8"
+            url: "https://adgem-framework.s3.amazonaws.com/iOS-2.4.1.zip",
+            checksum: "02ce8fcb168f8fedc0f09d430d0fde18e22966850cff6c4d04a247c136efd3e3"
         )
     ]
 )
